66341c4e53eea6fef6dd0d12ffb075fa84836da5
Volodymyr <vladimir.dvorakovskyi@gmail.com>
2023-02-26 22:48:46 +0200

    task-07 is done

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,22 +1,9 @@
 const inputEl = document.querySelector('#validation-input');
-// const handleInput = () => {
-//     if (inputEl.value === inputEl.dataset.length) {
-
-//     }
-// };
-// console.log(inputEl.dataset.length);
-
-
 inputEl.style.outline = '2px solid #bdbdbd';
-
-// inputEl.addEventListener('focus', (event) => {
-//     event.target.style.outline = '2px solid #bdbdbd';
-// })
 
 inputEl.addEventListener('blur', (event) => {
     if (event.target.value.trim().length === Number(event.target.dataset.length)) {
-        // event.target.style.borderColor = '#4caf50';
-        inputEl.classList.toggle('valid');
-    } else inputEl.classList.toggle('invalid');
-    // } else event.target.style.borderColor = '#f44336'
+        event.target.classList.remove('invalid');
+        event.target.classList.add('valid');
+    } else event.target.classList.add('invalid');
 })
