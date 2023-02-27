d9b3015944221005bda7fd15d5479c65c2a73e58
Volodymyr <vladimir.dvorakovskyi@gmail.com>
2023-02-27 16:43:27 +0200

    task-10 is done

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const numbersEl = document.querySelector('#controls input');
+const createBtnEl = document.querySelector('button[data-create]');
+const destroyBtnEl = document.querySelector('button[data-destroy]');
+const boxesEl = document.querySelector('#boxes');
+
+
+const createBoxes = (amount) => {
+  amount = numbersEl.value;
+  const arrayBoxes = [];
+  let defaultWidth = 30;
+  let defaultHeight = 30;
+
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement('div');
+    const hexColor = getRandomHexColor();
+    box.style.backgroundColor = hexColor;
+    box.style.width = `${defaultWidth}px`;
+    box.style.height = `${defaultHeight}px`;
+    defaultWidth += 10;
+    defaultHeight += 10;
+    arrayBoxes.push(box);
+  }
+
+  boxesEl.append(...arrayBoxes);
+}
+
+const destroyBoxes = () => {
+  boxesEl.innerHTML = '';
+}
+
+createBtnEl.addEventListener('click', createBoxes);
+destroyBtnEl.addEventListener('click', destroyBoxes);
