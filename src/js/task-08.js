f1c6c5c38df55d083672289f6dd3b6bb3f837e79
Volodymyr <vladimir.dvorakovskyi@gmail.com>
2023-02-27 10:27:11 +0200

    task-08 is done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,24 @@
+const formEl = document.querySelector('.login-form');
+const inputEls = formEl.querySelectorAll('input');
 
+formEl.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    inputEls.forEach((input) => {
+        if (input.value === '') {
+            alert('Будь ласка, заповніть усі поля');
+        }
+    })
+    
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+    const formData = {
+        email, password,
+    }
+
+    console.log(formData);
+
+    event.currentTarget.reset();
+})
